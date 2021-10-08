1fcb91fa8970bc072d09bdc1d18a28de6067d3ac
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-08 17:12:29 -0500

    Good morning!

--- step_1_data_resample.py
+++ step_1_data_resample.py
@@ -5,8 +5,11 @@ fileList = glob.glob("./*.nii.gz")
 fileList.sort()
 
 for filePath in fileList:
-	print(filePath)
+	# print(filePath)
 	fileName = os.path.basename(filePath)
 	# CT_011.nii.gz
 	fileIndex = fileName[-10:-7]
-	print(fileIndex)
+	# print(fileIndex)
+	cmd = "3dresample -dxyz 1.367 1.367 1.367 -prefix CUB_"+fileIndex+".nii.gz -input CT_"+fileIndex+".nii.gz"
+	print(cmd)
+	os.system(cmd)
