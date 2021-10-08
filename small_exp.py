71a4b0c048932fea13ec48b793d46376ba8fb8eb
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-08 16:37:33 -0500

    Good morning!

--- small_exp.py
+++ small_exp.py
@@ -1,8 +1,12 @@
 import nibabel as nib
 import numpy as np
+import torch.nn as nn
 
 import glob
 import os
+
+from monai.networks.nets import UNETR
+from torch.nn import Linear
 
 def normX(data):
     data[data<0] = 0
@@ -14,7 +18,26 @@ def normX(data):
 filePET = nib.load("./dataset/petTr/RS_011.nii.gz")
 dataPET = filePET.get_fdata()
 normPET = normX(dataPET)
-print(normPET.shape)
+halfPET = normPET[:, :, :700]
+print(halfPET.shape)
+# 700x700x700 343 patches
 
 # try to input it to the UNETR model
+
+model = UNETR(
+    in_channels=1,
+    out_channels=1,
+    img_size=(100, 100, 100),
+    feature_size=16,
+    hidden_size=768,
+    mlp_dim=3072,
+    num_heads=12,
+    pos_embed="perceptron",
+    norm_name="instance",
+    res_block=True,
+    dropout_rate=0.0,
+)
+model.add_module("linear", nn.Linear(in_features = opt.block_size, 
+                                     out_features = opt.block_size)).to(device)
+
 # try to get the output
