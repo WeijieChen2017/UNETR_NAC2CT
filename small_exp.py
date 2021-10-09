af9d6115fe4c636fb4c276e253dca1b0d4820220
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-09 00:01:38 -0500

    Good morning!

--- small_exp.py
+++ small_exp.py
@@ -17,54 +17,93 @@ def normX(data):
     data = data / 6000
     return data
 
+def normY(data):
+    data[data<-1000] = -1000
+    data[data>3000] = 3000
+    data = (data + 1000) / 4000
+    return data
+
 batch_size = 16
+loss_batch_cnt = 16
+widthZ = 8
 
 
-# device = torch.device("cuda")
-# model = UNETR(
-#     in_channels=1,
-#     out_channels=1,
-#     img_size=(lx, ly, lz),
-#     feature_size=256,
-#     hidden_size=4096,
-#     mlp_dim=4096,
-#     num_heads=16,
-#     pos_embed="perceptron",
-#     norm_name="instance",
-#     res_block=True,
-#     dropout_rate=0.0,
-# ).half().to(device)
+device = torch.device("cuda")
+model = UNETR(
+    in_channels=1,
+    out_channels=1,
+    img_size=(lx, ly, lz),
+    feature_size=256,
+    hidden_size=4096,
+    mlp_dim=4096,
+    num_heads=16,
+    pos_embed="perceptron",
+    norm_name="instance",
+    res_block=True,
+    dropout_rate=0.0,
+).half().to(device)
 
-# loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
-# torch.backends.cudnn.benchmark = True
-# optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
-# model.train()
+loss_function = DiceCELoss(to_onehot_y=True, softmax=True)
+torch.backends.cudnn.benchmark = True
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+model.train()
 
 
 # try to load one data
-filePET = nib.load("./dataset/sctTr/CUB_011.nii.gz")
-# dataPET = filePET.get_fdata()[:, :, idx-8:idx+8]
+filePET = nib.load("./dataset/petTr/CUB_011.nii.gz")
+fileSCT = nib.load("./dataset/sctTr/CUB_011.nii.gz")
 dataPET = filePET.get_fdata()[:, :, -512:]
+dataSCT = fileSCT.get_fdata()[:, :, -512:]
 normPET = normX(dataPET)
+normSCT = normY(dataSCT)
 hx, hy, hz = normPET.shape
 lx, ly, lz = hx//2, hy//2, hz//2
 normPET = np.resize(normPET, (lx, ly, lz))
+normSCT = np.resize(normSCT, (lx, ly, lz))
 normPET = np.expand_dims(normPET, axis=(0, 1))
-print(normPET.shape)
+normSCT = np.expand_dims(normSCT, axis=(0, 1))
+print(normPET.shape, normSCT.shape)
 
 cntBatch = 0
-inputBatch = np.zeros((batch_size, 1, lx, ly, 8))
+inputBatchX = np.zeros((batch_size, 1, lx, ly, widthZ))
+inputBatchY = np.zeros((batch_size, 1, lx, ly, widthZ))
+lenData = lz // widthZ
 
-for idz in range(lz//8):
-	sliceBatch = normPET[:, :, :, :, idz*8 : idz*8+8]
-	print(sliceBatch.shape)
-	inputBatch[cntBatch, :, :, :, :] = sliceBatch
-	cntBatch += 1
 
-	if cntBatch == 16:
-		print(inputBatch.shape)
-		# train an epoch
-		cntBatch = 0
+# epoch_loss = np.zeros((lenData))
+# loss_batch = np.zeros((loss_batch_cnt))
+
+for idz in range(lz//widthZ):
+    sliceBatchX = normPET[:, :, :, :, idz*widthZ : idz*8+widthZ]
+    sliceBatchY = normSCT[:, :, :, :, idz*widthZ : idz*8+widthZ]
+    inputBatchX[cntBatch, :, :, :, :] = sliceBatchX
+    inputBatchY[cntBatch, :, :, :, :] = sliceBatchY
+    cntBatch += 1
+
+    if cntBatch == 16:
+        
+        realInputX = torch.from_numpy(inputBatchX).half().to(device)
+        realInputY = torch.from_numpy(inputBatchY).half().to(device)
+        optimizer.zero_grad()
+        loss = criterion(model(batch_x), batch_y)
+        loss.backward()
+        optimizer.step()
+        loss_voxel = loss.item() / (lx*ly*widthZ)
+
+        loss_mean = np.mean(loss_voxel)
+        loss_std = np.std(loss_voxel)
+        print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
+
+        # loss_batch[iteration % loss_batch_cnt] = loss_voxel
+        # epoch_loss[iteration] = loss_voxel
+
+        # if iteration % loss_batch_cnt == loss_batch_cnt - 1:
+        #     loss_mean = np.mean(loss_batch)
+        #     loss_std = np.std(loss_batch)
+        #     print("===> Epoch[{}]({}/{}): ".format(epoch + 1, iteration + 1, len(data_loader)), end='')
+        #     print("Loss mean: {:.6} Loss std: {:.6}".format(loss_mean, loss_std))
+
+        cntBatch = 0
 
 
 
