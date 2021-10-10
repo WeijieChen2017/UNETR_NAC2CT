8d8c44643e320cffcd831577c1fc8ff4c4a3e519
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-10 00:36:49 -0500

    Good morning!

--- small_exp.py
+++ small_exp.py
@@ -42,6 +42,7 @@ def hook_backward_fn(module, grad_input, grad_output):
             print(f"grad_input: {grad_input}")
             print("*"*20)
         else:
+            print("===============================>Vit<===============================")
             print(f"grad_output: {grad_output}")
             print(f"grad_input: {grad_input}")
             print("*"*20)
