18e4174dfbc0a7d2ea13f1dd64e14d634a0eb9f8
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-16 19:19:51 -0500

    Good morning!

--- SwinIR/main_train_swinir.py
+++ SwinIR/main_train_swinir.py
@@ -59,8 +59,8 @@ def main():
     criterion = nn.SmoothL1Loss()
     optimizer = torch.optim.AdamW(model.parameters())
 
-    sct_list = sorted(glob.glob(args.folder_sct))
-    sct_list_v = sorted(glob.glob(args.folder_sct_v))
+    sct_list = sorted(glob.glob(args.folder_sct+".npy"))
+    sct_list_v = sorted(glob.glob(args.folder_sct_v+".npy"))
     train_loss = np.zeros((args.epoch))
     epoch_loss = np.zeros((len(sct_list)))
     epoch_loss_v = np.zeros((len(sct_list)))
@@ -69,7 +69,7 @@ def main():
     case_loss = None
 
     for idx_epoch in range(args.epoch):
-        print("~~~~~~Epoch[{:03d}]~~~~~~".format(idx_epoch))
+        print("~~~~~~Epoch[{:03d}]~~~~~~".format(idx_epoch), end="")
 
         # ====================================>train<====================================
         model.train()
