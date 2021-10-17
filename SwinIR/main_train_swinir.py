e07f2b2a5d7e317f9ebed4bdfd84ffb1481938ba
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-16 22:46:44 -0500

    Good morning!

--- SwinIR/main_train_swinir.py
+++ SwinIR/main_train_swinir.py
@@ -35,7 +35,7 @@ def main():
     parser.add_argument('--tile_overlap', type=int, default=32, help='Overlapping of different tiles')
     
     parser.add_argument('--epoch', type=int, default=10, help='how many epochs to train')
-    parser.add_argument('--batch', type=int, default=4, help='how many batches in one run')
+    parser.add_argument('--batch', type=int, default=1, help='how many batches in one run')
     parser.add_argument('--loss_display_per_iter', type=int, default=8, help='display how many losses per iteration')
     parser.add_argument('--folder_pet', type=str, default="./trainsets/X/train/", help='input folder of NAC PET images')
     parser.add_argument('--folder_sct', type=str, default="./trainsets/Y/train/", help='input folder of sCT images')
