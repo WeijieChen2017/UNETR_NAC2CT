6dddd7daae09d47f141b4bfd1bf536ce9d1e7e8a
WeijieChen2017 <weijiechen1997@gmail.com>
2021-10-20 23:58:15 -0500

    Good morning!

--- step_1_data_resample.py
+++ step_1_data_resample.py
@@ -1,15 +1,20 @@
 import os
 import glob
 
-fileList = glob.glob("./*.nii.gz")
-fileList.sort()
+# fileList = glob.glob("./*.nii.gz")
+# fileList.sort()
 
-for filePath in fileList:
-	# print(filePath)
-	fileName = os.path.basename(filePath)
-	# CT_011.nii.gz
-	fileIndex = fileName[-10:-7]
-	# print(fileIndex)
-	cmd = "3dresample -dxyz 5.468 5.468 5.468 -prefix RSZ_"+fileIndex+".nii.gz -input CUB_"+fileIndex+".nii.gz"
-	print(cmd)
-	os.system(cmd)
+# for filePath in fileList:
+#     # print(filePath)
+#     fileName = os.path.basename(filePath)
+#     # CT_011.nii.gz
+#     fileIndex = fileName[-10:-7]
+#     # print(fileIndex)
+#     cmd = "3dresample -dxyz 2 2 1 -prefix RSZ_"+fileIndex+".nii.gz -input CUB_"+fileIndex+".nii.gz"
+#     print(cmd)
+#     os.system(cmd)
+
+for idx in range(64):
+    cmd = "3dresample -dxyz 2 2 1 -prefix RSZ_"+fileIndex+".nii.gz -input MR__MLAC_"+str(idx)+"_MNI.nii.gz"
+    print(cmd)
+    os.system(cmd)
